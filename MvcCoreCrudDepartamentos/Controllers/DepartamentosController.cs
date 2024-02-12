5ae94aac6e6f07af6c95e2f345ea3216ee09359c
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-12 11:10:34 +0100

    Commit

---
+++ MvcCoreCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCrudDepartamentos.Models;
+using MvcCoreCrudDepartamentos.Repositories;
+
+namespace MvcCoreCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        RepositoryDepartamentos repo;
+
+        public DepartamentosController()
+        {
+            this.repo = new RepositoryDepartamentos();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Departamento departamento = await this.repo.FindDepartamentoAsync(id);
+            return View(departamento);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string nombre, string localidad)
+        {
+            await this.repo.InsertDepartamentoAsync(nombre, localidad);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            Departamento departamento = await this.repo.FindDepartamentoAsync(id);
+            return View(departamento);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Departamento departamento)
+        {
+            await this.repo.UpdateDepartamentosAsync(departamento.IdDepartamento,
+                departamento.Nombre, departamento.Localidad);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.repo.DeleteDepartamentoAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreCrudDepartamentos/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,125 @@
+﻿using MvcCoreCrudDepartamentos.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+#region PROCEDIMIENTOS ALMACENADOS
+/*
+CREATE PROCEDURE SP_INSERTDEPARTAMENTO
+(@NOMBRE NVARCHAR(50), @LOCALIDAD NVARCHAR(50))
+AS
+    DECLARE @NEXTID INT
+    SELECT @NEXTID = MAX(DEPT_NO) + 1
+    FROM DEPT
+    INSERT INTO DEPT
+    VALUES(@NEXTID, @NOMBRE, @LOCALIDAD)
+GO
+*/
+#endregion
+
+namespace MvcCoreCrudDepartamentos.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryDepartamentos()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=sa;Password=MCSD2023;Encrypt=False";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        // Vamos a realizar las consultas sobre la BBDD
+        // de forma asíncrona, con async/await
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            string sql = "SELECT * FROM DEPT";
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            List<Departamento> departamentos = new List<Departamento>();
+            while (await this.reader.ReadAsync())
+            {
+                Departamento dept = new Departamento();
+                dept.IdDepartamento = int.Parse(this.reader["DEPT_NO"].ToString());
+                dept.Nombre = this.reader["DNOMBRE"].ToString();
+                dept.Localidad = this.reader["LOC"].ToString();
+                departamentos.Add(dept);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            return departamentos;
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        {
+            string sql = "SELECT * FROM DEPT WHERE DEPT_NO = @ID";
+            this.com.Parameters.AddWithValue("@ID", id);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            // Al buscar, debemos averiguar si existen datos o no
+            // en el repository. Siempre que no existan, el repo
+            // debe devolver NULL
+            Departamento departamento = null;
+            if (await this.reader.ReadAsync())
+            {
+                departamento = new Departamento();
+                departamento.IdDepartamento = int.Parse(this.reader["DEPT_NO"].ToString());
+                departamento.Nombre = this.reader["DNOMBRE"].ToString();
+                departamento.Localidad = this.reader["LOC"].ToString();
+            }
+            else
+            {
+
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+            return departamento;
+        }
+
+        public async Task InsertDepartamentoAsync(string nombre, string localidad)
+        {
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = "SP_INSERTDEPARTAMENTO";
+            this.com.Parameters.AddWithValue("@NOMBRE", nombre);
+            this.com.Parameters.AddWithValue("@LOCALIDAD", localidad);
+            await this.cn.OpenAsync();
+            int result = await this.com.ExecuteNonQueryAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+
+        public async Task UpdateDepartamentosAsync(int id, string nombre, string localidad)
+        {
+            string sql = "UPDATE DEPT SET DNOMBRE = @NOMBRE, LOC = @LOCALIDAD WHERE DEPT_NO = @ID";
+            this.com.Parameters.AddWithValue("@NOMBRE", nombre);
+            this.com.Parameters.AddWithValue("@LOCALIDAD", localidad);
+            this.com.Parameters.AddWithValue("@ID", id);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            int result = await this.com.ExecuteNonQueryAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+
+        public async Task DeleteDepartamentoAsync(int id)
+        {
+            string sql = "DELETE FROM DEPT WHERE DEPT_NO = @ID";
+            this.com.Parameters.AddWithValue("@ID", id);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            int result = await this.com.ExecuteNonQueryAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+    }
+}
