df679ad3c74d50c08ee1a9556f725720a259c6d8
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-12 09:09:28 +0100

    Add project files.

---
+++ MvcCoreCrudDepartamentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCoreCrudDepartamentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
